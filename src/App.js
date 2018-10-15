6781951c30df1d09002ad4a68379ea13b9b52fff
Jose Luis Barahona <jolubax@icloud.com>
2018-10-15 16:22:40 +0200

    Checkpoint chat-list-rooms: Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import * as firebase from 'firebase';
 import React, { Component } from 'react';
+import RoomList from './components/RoomList';
 import './App.css';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDa0wveyGiBrYjWEafAFBB6fRCOMiOzB-8",
+  authDomain: "react-chat-7f917.firebaseapp.com",
+  databaseURL: "https://react-chat-7f917.firebaseio.com",
+  projectId: "react-chat-7f917",
+  storageBucket: "react-chat-7f917.appspot.com",
+  messagingSenderId: "149441008954"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
