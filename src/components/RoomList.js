9df0fbf4a7d517334bd1d0bec81a436b234405d9
Jose Luis Barahona <jolubax@icloud.com>
2018-10-16 07:58:36 +0200

    Checkpoint chat-create-rooms: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,40 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 
 class RoomList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rooms: []
-        };
-        this.roomsRef = this.props.firebase.database().ref('rooms');
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+    this.roomsRef = this.props.firebase.database().ref("rooms");
+  }
 
-    componentDidMount() {
-        this.roomsRef.on('child_added', snapshot => {
-            const room = snapshot.val();
-            room.key = snapshot.key;
-            this.setState({
-                rooms: this.state.rooms.concat(room)
-            });
-        });
-    }
+  componentDidMount() {
+    this.roomsRef.on("child_added", snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({
+        rooms: this.state.rooms.concat(room)
+      });
+    });
+  }
 
-    render() {
-        return (
-            <section className="rooms-list">
-                <ul className="current-list">
-                {this.state.rooms.map( room => {
-                    return (
-                        <li key={room.key}>
-                            {room.name}
-                        </li>
-                    );
-                })}
-                </ul>
-            </section>
-        );
-    }
+  createRoom(e) {
+    e.preventDefault();
+    const newRoomName = this.newRoomName.value;
+    this.roomsRef.push({
+      name: newRoomName
+    });
+  }
+
+  render() {
+    return (
+      <section className="rooms-list">
+        <ul className="current-list">
+          {this.state.rooms.map(room => {
+            return <li key={room.key}>{room.name}</li>;
+          })}
+        </ul>
+        <form className="create-room" onSubmit={e => this.createRoom(e)}>
+          <label>
+            Add a new chat room:
+            <input type="text" ref={e => (this.newRoomName = e)} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </section>
+    );
+  }
 }
 
 export default RoomList;
